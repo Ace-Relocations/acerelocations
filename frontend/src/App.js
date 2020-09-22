37793f19eedd5068c32ada3d6d323eedf98a4689
shaivpidadi <shaivpidadi@gmail.com>
2020-09-22 18:00:11 +0530

    setup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 import { PDFViewer } from "@react-pdf/renderer";
 import Invoice from "./Test/Invoice";
 import invoice from "./data/invoice";
@@ -9,10 +10,14 @@ import CustomerInfoForm from "./components/CustomerInfoForm/CustomerInfoForm";
 function App() {
   return (
     <Fragment>
+      <Switch>
+        <Route path="/customer" component={CustomerInfoForm} />
+        <Route path="/" exact render={() => <Redirect to="/customer" />} />
+      </Switch>
       {/* <PDFViewer width="1000" height="1000" className="app">
         <Invoice invoice={invoice} />
-      </PDFViewer> */}
-      <CustomerInfoForm />
+      </PDFViewer>
+      <CustomerInfoForm /> */}
     </Fragment>
   );
 }
