f4a1543e265ad714c3bb7841e8c8d34e10a5cbc2
shaivpidadi <shaivpidadi@gmail.com>
2020-09-24 00:33:41 +0530

    Added dynamic pdf edit

--- frontend/src/Test/Invoice.js
+++ frontend/src/Test/Invoice.js
@@ -123,87 +123,96 @@ const address = [
   { line: 'ANDHRA PRADESH-515001' },
 ];
 
-const Invoice = ({ invoice }) => (
-  <Document>
-    <Page size='A4' style={styles.page} orientation='landscape'>
-      <View>
-        <View style={styles.container} wrap={false}>
-          <Image style={styles.logo} src={logo} />
-          <View wrap={false} style={styles.width90}>
-            <Text style={styles.headerText}>We Make Moving Easy</Text>
-            <Text style={styles.headerSubText}>
-              6TH LANE,2ND CROSS BHAGYANAGAR, KALYANDURGAM ROAD,ANANTAPUR, ANDHRA PRADESH-515001
-            </Text>
-            <Text style={styles.headerSubText}>
-              E: shaivpidadi@gmail.com W: www.shaivpidadi.com
-            </Text>
-          </View>
-        </View>
-        <View
-          style={{
-            height: '500px',
-            flexDirection: 'row',
-            flexGrow: 1,
-          }}
-        >
-          <View
-            style={{
-              height: '400px',
-              width: '70%',
-            }}
-          >
-            <View
-              style={{
-                // border: 1,
-                width: '100%',
-                flexDirection: 'row',
-                marginBottom: '10px',
-              }}
-            >
-              <View style={{ width: '70%' }}>
-                <Boxwith4Line title='Cognisor' address={address} fullName='Mr. MILAN SOLANKI' />
-              </View>
-              <View style={{ width: '30%', border: '1 1 1 0' }}>
-                <BoxWith4LineKeys gsnNo='2313' date='12-10-3123' from='Shaishav' to='KnowOne' />
-              </View>
-            </View>
-            <View style={{ marginBottom: '10px' }}>
-              <Boxwith4Line title='Consignee' address={address} fullName='Mr. MILAN SOLANKI' />
-            </View>
-
-            <CustomerPackageDesTable
-              description='This is very looooooooooooooooooooooooooooong Description'
-              noOfPkg={10}
-            />
-
-            <View>
-              <Text style={{ color: 'red', fontSize: 7, marginLeft: 2 }}>
-                Note : Corrugated boxes, PVC boxes, Fiber drums & Moving blankets are property of
-                ACE Relocations
+const Invoice = ({ invoice }) => {
+  console.log(invoice);
+  const { firstName, lastName } = invoice;
+  console.log(firstName);
+  return (
+    <Document>
+      <Page size='A4' style={styles.page} orientation='landscape'>
+        <View>
+          <View style={styles.container} wrap={false}>
+            <Image style={styles.logo} src={logo} />
+            <View wrap={false} style={styles.width90}>
+              <Text style={styles.headerText}>We Make Moving Easy</Text>
+              <Text style={styles.headerSubText}>
+                6TH LANE,2ND CROSS BHAGYANAGAR, KALYANDURGAM ROAD,ANANTAPUR, ANDHRA PRADESH-515001
+              </Text>
+              <Text style={styles.headerSubText}>
+                E: shaivpidadi@gmail.com W: www.shaivpidadi.com
               </Text>
             </View>
           </View>
           <View
             style={{
-              height: '400px',
-              width: '30%',
-              alignItems: 'center',
-              padding: 3,
+              height: '500px',
+              flexDirection: 'row',
+              flexGrow: 1,
             }}
           >
-            <Text style={styles.title}> CUSTOMER COPY</Text>
-            <ConsignmentBox />
-            <AcknowledgementTable fullName='Shaishav Pidadi' number='12' />
-            <AffliationBox />
-            <View style={{ marginTop: 5 }}>
-              <Text style={{ color: 'red', fontSize: 8 }}>Subject to Ahmedabad jurisdiction</Text>
+            <View
+              style={{
+                height: '400px',
+                width: '70%',
+              }}
+            >
+              <View
+                style={{
+                  // border: 1,
+                  width: '100%',
+                  flexDirection: 'row',
+                  marginBottom: '10px',
+                }}
+              >
+                <View style={{ width: '70%' }}>
+                  <Boxwith4Line
+                    title='Cognisor'
+                    address={address}
+                    fullName={`${firstName} ${lastName}`}
+                  />
+                </View>
+                <View style={{ width: '30%', border: '1 1 1 0' }}>
+                  <BoxWith4LineKeys gsnNo='2313' date='12-10-3123' from='Shaishav' to='KnowOne' />
+                </View>
+              </View>
+              <View style={{ marginBottom: '10px' }}>
+                <Boxwith4Line title='Consignee' address={address} fullName='Mr. MILAN SOLANKI' />
+              </View>
+
+              <CustomerPackageDesTable
+                description='This is very looooooooooooooooooooooooooooong Description'
+                noOfPkg={10}
+              />
+
+              <View>
+                <Text style={{ color: 'red', fontSize: 7, marginLeft: 2 }}>
+                  Note : Corrugated boxes, PVC boxes, Fiber drums & Moving blankets are property of
+                  ACE Relocations
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                height: '400px',
+                width: '30%',
+                alignItems: 'center',
+                padding: 3,
+              }}
+            >
+              <Text style={styles.title}> CUSTOMER COPY</Text>
+              <ConsignmentBox />
+              <AcknowledgementTable fullName='Shaishav Pidadi' number='12' />
+              <AffliationBox />
+              <View style={{ marginTop: 5 }}>
+                <Text style={{ color: 'red', fontSize: 8 }}>Subject to Ahmedabad jurisdiction</Text>
+              </View>
             </View>
           </View>
         </View>
-      </View>
-    </Page>
-  </Document>
-);
+      </Page>
+    </Document>
+  );
+};
 
 // const Invoice = ({ invoice }) => (
 //   <Document>
