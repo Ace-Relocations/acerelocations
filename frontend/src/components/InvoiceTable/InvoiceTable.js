64c4e374933c1f4de7b21a32eff018397f30ea33
shaivpidadi <shaivpidadi@gmail.com>
2020-09-26 14:24:31 +0530

    Added FooterWithImage

--- frontend/src/components/InvoiceTable/InvoiceTable.js
+++ frontend/src/components/InvoiceTable/InvoiceTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Image, Text, View, StyleSheet } from '@react-pdf/renderer';
-
-import footer from '../../footer.png';
+import { Text, View, StyleSheet } from '@react-pdf/renderer';
+import FooterWithImage from '../FooterWithImage/FooterWithImage';
 
 const styles = StyleSheet.create({
   container: {
@@ -132,9 +131,8 @@ const InvoiceTable = () => {
         <View style={{ border: 1 }} />
         <View style={{ border: '1 solid red', marginTop: '2px' }} />
       </View>
-      <View style={{ marginTop: 5 }}>
-        <Image src={footer} />
-      </View>
+
+      <FooterWithImage />
     </View>
   );
 };
