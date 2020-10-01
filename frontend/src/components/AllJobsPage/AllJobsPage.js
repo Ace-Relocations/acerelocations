dd7031ecfa46812bc73236da8b9820d946b64c35
shaivpidadi <shaivpidadi@gmail.com>
2020-10-02 00:54:02 +0530

    All Jobs page table init

--- frontend/src/components/AllJobsPage/AllJobsPage.js
+++ frontend/src/components/AllJobsPage/AllJobsPage.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid/index';
+import Typography from '@material-ui/core/Typography';
 
+import AllJobsTable from '../AllJobsTable/AllJobsTable';
+
+const useStyles = makeStyles((theme) => ({
+  introBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '130px',
+    marginTop: '60px',
+    fontFamily: 'Roboto Mono, monospace',
+  },
+  primaryText: {
+    fontSize: '35px',
+    fontWeight: 'bold',
+  },
+  secondaryText: {
+    position: 'relative',
+    top: '-10px',
+    opacity: '.78',
+  },
+  formBox: {
+    dispaly: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(5),
+  },
+}));
 const AllJobsPage = () => {
-  return <div>AllJobsPage</div>;
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item lg={12} container className={classes.gridItem}>
+        <Box className={classes.introBox}>
+          <div>
+            <Typography variant='h3' component='h1' className={classes.primaryText}>
+              All Jobs
+              <Typography variant='h3' className={classes.primaryText}></Typography>
+              <Typography variant='h6' component='span' className={classes.secondaryText}>
+                List of all Jobs
+              </Typography>
+            </Typography>
+          </div>
+        </Box>
+      </Grid>
+      <Grid item lg={12} container className={classes.gridItem}>
+        <Box component='div'>
+          <AllJobsTable />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default AllJobsPage;
--- frontend/src/components/AllJobsTable/AllJobsTable.js
+++ frontend/src/components/AllJobsTable/AllJobsTable.js
@@ -3,3 +3,5 @@ import React from 'react';
 const AllJobsTable = () => {
   return <div>AllJobsTable</div>;
 };
+
+export default AllJobsTable;
