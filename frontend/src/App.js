2fc50c84092bd3acf3fe59ecf7b510ef167188f0
shaivpidadi <shaivpidadi@gmail.com>
2020-10-01 19:20:14 +0530

    setup homepage and sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,56 @@
 import React, { Fragment } from 'react';
-import { Switch, Route, Redirect, Link } from 'react-router-dom';
-import { PDFViewer, Page, StyleSheet, Document, View } from '@react-pdf/renderer';
-import Invoice from './Test/Invoice';
-import invoice from './data/invoice';
-import MultiStep from './components/MultiStep/MultiStep';
+import { Switch, Route, Redirect, Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 
 import './App.css';
+import axios from './services';
 import CustomerInfoForm from './components/CustomerInfoForm/CustomerInfoForm';
-import Header from './components/Header/Header';
-import CustomerFeedbackForm from './components/CustomerFeedbackForm/CustomerFeedbackForm';
-import LuggageList from './components/LuggageList/LuggageList';
-import HeaderWithAddress from './components/HeaderWithAddress/HeaderWithAddress';
-import Reciept from './components/Reciept/Reciept';
-import InvoiceTable from './components/InvoiceTable/InvoiceTable';
-import FooterWithImage from './components/FooterWithImage/FooterWithImage';
-import TransitPlanFormBox from './components/TransitPlanFormBox/TransitPlanFormBox';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import 'react-toastify/dist/ReactToastify.css';
+import HomePage from './components/HomePage/HomePage';
+import Sidebar from './components/Sidebar/Sidebar';
 
 toast.configure();
 
-const styles = StyleSheet.create({
-  page: {
-    fontFamily: 'Helvetica',
-    fontSize: 11,
-    paddingTop: 30,
-    paddingLeft: 60,
-    paddingRight: 60,
-    lineHeight: 1.5,
-    flexDirection: 'column',
-  },
-});
-
 function App() {
+  const history = useHistory();
+  const location = useLocation();
+  const redirect = !false;
+
+  (function () {
+    const authorizationToken = localStorage.getItem('userToken');
+    const token = 'Bearer ' + authorizationToken;
+    console.log({ token });
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+    } else {
+      axios.defaults.headers.common.Authorization = null;
+    }
+  })();
+
+  const dispatch = useDispatch();
+  const authUser = useSelector((state) => state.Auth.authUser);
+
+  console.log({ authUser });
+  if (
+    location.pathname != '/login' &&
+    (authUser == null || authUser == undefined || authUser == 'undefined')
+  ) {
+    history.push('/login');
+  } else if (location.pathname == '/login' && authUser) {
+    history.push('/');
+  }
+
   return (
     <Fragment>
+      <Sidebar />
       <Switch>
         <Route path='/customer' component={CustomerInfoForm} />
         <Route path='/login' component={Login} />
-        <Route path='/' exact render={() => <Redirect to='/login' />} />
+        <Route path='/' component={HomePage} />
+        {/* <Route path='/' exact render={() => <Redirect to='/login' />} /> */}
 
         {/* Hiding Registration temporary */}
         {/* <Route path='/signup' component={Signup} /> */}
