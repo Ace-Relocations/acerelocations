a6b89fd9ed4972bbd1b6ccaa8b0012f0d109cb5d
shaivpidadi <shaivpidadi@gmail.com>
2020-10-10 01:32:02 +0530

    Added checkbox in invoice

--- frontend/src/components/AddInvoiceDialog/AddInvoiceDialog.js
+++ frontend/src/components/AddInvoiceDialog/AddInvoiceDialog.js
@@ -7,8 +7,6 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import Draggable from 'react-draggable';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Slide from '@material-ui/core/Slide';
 import CloseIcon from '@material-ui/icons/Close';
@@ -20,6 +18,13 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid/index';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -46,8 +51,40 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 const AddInvoiceDialog = ({ openInvoice, handleAddInvoice, handleCancleInvoice }) => {
   const classes = useStyles();
 
-  const [fields, setFields] = useState([{ expense: null, amount: null }]);
+  const [fields, setFields] = useState([
+    {
+      expense:
+        'Transportation charges inclusive of allied services for your personal effects and car from ORIGIN to DESTINATION',
+      amount: null,
+      isChecked: false,
+    },
+    {
+      expense:
+        'Transportation charges inclusive of allied services for your personal effects from ORIGIN to DESTINATION',
+      amount: null,
+      isChecked: false,
+    },
+    {
+      expense: 'Transportation charges for your Office effects from ORIGIN to DESTINATION',
+      amount: null,
+      isChecked: false,
+    },
+    {
+      expense: 'Others',
+      amount: null,
+      isChecked: false,
+    },
+  ]);
+  const [state, setState] = useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+  const { gilad, jason, antoine } = state;
+
+  console.log({ fields });
   const [validateExpenses, updateValidateExpenses] = useState(false);
+
   const handleChange = (i, event) => {
     const values = [...fields];
     values[i].expense = event.target.value;
@@ -57,6 +94,11 @@ const AddInvoiceDialog = ({ openInvoice, handleAddInvoice, handleCancleInvoice }
   const handleChangeAmount = (i, event) => {
     const values = [...fields];
     values[i].amount = event.target.value;
+    setFields(values);
+  };
+  const handleChangeCheckbox = (i, event) => {
+    const values = [...fields];
+    values[i].isChecked = !values[i].isChecked;
     setFields(values);
   };
 
@@ -73,17 +115,17 @@ const AddInvoiceDialog = ({ openInvoice, handleAddInvoice, handleCancleInvoice }
   };
 
   useEffect(() => {
-    const isValid = !Object.values(fields).some(({ expense, amount }) => {
+    const isValid = Object.values(fields).some(({ isChecked, amount }) => {
       if (fields.length < 1) {
         return false;
       }
-
-      return !((expense !== null && amount !== null) || (expense === '' && amount === 0));
+      return (isChecked == true && amount > 0) || (isChecked === false && amount === 0);
     });
     updateValidateExpenses(isValid);
   }, [fields]);
 
-  const isValid = fields.length > 0 && validateExpenses;
+  // const isValid = fields.length > 0 && validateExpenses;
+  const isValid = fields.length;
 
   return (
     <Dialog
@@ -120,23 +162,34 @@ const AddInvoiceDialog = ({ openInvoice, handleAddInvoice, handleCancleInvoice }
           {fields.map((field, idx) => {
             return (
               <div key={`${field}-${idx}`} style={{ marginBottom: '10px' }}>
-                <Grid container lg={7}>
-                  <Grid item lg={5} container className={classes.gridItem}>
-                    <Box marginRight='10px'>
-                      <TextField
+                <Grid container lg={12}>
+                  <Grid item lg={6} md={6} container className={classes.gridItem}>
+                    <Box marginRight='10px' fontSize='10px'>
+                      {/* <TextField
                         type='text'
                         placeholder='Enter Expense'
                         label='Expense'
-                        variant='outlined'
+                        // variant='outlined'
+                        // rows={2}
+                        // multiline
                         name='expense'
                         value={field.expense || ''}
                         onChange={(e) => handleChange(idx, e)}
-                        required
+                        style={{ width: '600px' }}
+                        disabled
+                      /> */}
+                      <TextareaAutosize
+                        aria-label='Invoice'
+                        rowsMin={3}
+                        name='expense'
+                        value={field.expense || ''}
+                        disabled
+                        style={{ width: '400px' }}
                       />
                     </Box>
                   </Grid>
-                  <Grid item lg={5} container className={classes.gridItem}>
-                    <Box>
+                  <Grid item lg={4} md={4} container>
+                    <Box width={150}>
                       <TextField
                         type='number'
                         placeholder='Enter Amount'
@@ -146,22 +199,38 @@ const AddInvoiceDialog = ({ openInvoice, handleAddInvoice, handleCancleInvoice }
                         value={field.amount || ''}
                         onChange={(e) => handleChangeAmount(idx, e)}
                         required
+                        style={{ width: '150px !important' }}
+                        inputStyle={{ width: '150px !important' }}
+                        fullWidth={false}
+                        disabled={!fields[idx].isChecked}
                       />
                     </Box>
                   </Grid>
-                  <Grid item lg={2} container className={classes.gridItem}>
-                    <Button type='button' onClick={() => handleRemove(idx)}>
+                  <Grid item lg={2} md={2} container className={classes.gridItem}>
+                    {/* <Button type='button' onClick={() => handleRemove(idx)}>
                       REMOVE
-                    </Button>
+                    </Button> */}
+                    <FormGroup>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={fields[idx]?.isChecked || false}
+                            onChange={(event) => handleChangeCheckbox(idx, event)}
+                            name='add'
+                          />
+                        }
+                        label='Add'
+                      />
+                    </FormGroup>
                   </Grid>
                 </Grid>
               </div>
             );
           })}
 
-          <Button type='button' onClick={() => handleAdd()} marginBottom='10px'>
+          {/* <Button type='button' onClick={() => handleAdd()} marginBottom='10px'>
             Add More
-          </Button>
+          </Button> */}
         </DialogContentText>
       </DialogContent>
       <DialogActions>
