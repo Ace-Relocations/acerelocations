78f722357add6c31f59abed5680c713fc0c8b7c7
shaivpidadi <shaivpidadi@gmail.com>
2020-10-11 19:14:04 +0530

    TellySheet Added

--- frontend/src/components/TellySheet/TellySheet.js
+++ frontend/src/components/TellySheet/TellySheet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page, Document, Image, StyleSheet, Text, View } from '@react-pdf/renderer';
 import Header from '../Header/Header';
+import FooterWithImage from '../FooterWithImage/FooterWithImage';
 
 const styles = StyleSheet.create({
   page: {
@@ -108,6 +109,217 @@ const TellySheet = () => {
     <Document>
       <Page size='A4' style={styles.page}>
         <Header />
+        <View>
+          <View style={{ marginTop: '10px' }}>
+            <Text style={{ textAlign: 'center' }}>TALLY SHEET</Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              textAlign: 'center',
+              border: 1,
+              fontSize: 9,
+              height: 20,
+              marginTop: '5px',
+            }}
+          >
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>CONSIGNOR</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '30.44%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>Mr. Parag Somani</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '13%' }}>
+              <Text style={{ marginVertical: 'auto' }}>LOCATION</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '13.33%' }}>
+              <Text style={{ marginVertical: 'auto' }}>Ahmedabad</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '13.33%' }}>
+              <Text style={{ marginVertical: 'auto' }}>MODE</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '13.33%' }}>
+              <Text style={{ marginVertical: 'auto' }}>Road</Text>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              textAlign: 'center',
+              borderRight: 1,
+              borderLeft: 1,
+              fontSize: 9,
+              height: 20,
+            }}
+          >
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>GCN NUMBER</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>M010454</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>JOB NO.</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>?</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>ORIGIN</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>Ahmedabad</Text>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              textAlign: 'center',
+              border: '1',
+              fontSize: 9,
+              height: 20,
+            }}
+          >
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>GCN DATE</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ textAlign: 'center', marginVertical: 'auto' }}>14.02.2020</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>JOB DATE</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>14.02.2020</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>DESTINATION</Text>
+            </View>
+            <View style={{ borderRight: 1, width: '16.66%' }}>
+              <Text style={{ marginVertical: 'auto' }}>Mumbai</Text>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: '10px',
+            }}
+          >
+            <View style={{ borderRight: 1, width: '30%' }}></View>
+            <View
+              style={{
+                width: '70%',
+                flexDirection: 'row',
+                textAlign: 'center',
+                border: 1,
+                fontSize: 9,
+                height: 20,
+              }}
+            >
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ textAlign: 'left', marginVertical: 'auto' }}>TOTAL PACKAGES</Text>
+              </View>
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ textAlign: 'center', marginVertical: 'auto' }}></Text>
+              </View>
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ marginVertical: 'auto' }}>VOLUME</Text>
+              </View>
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ marginVertical: 'auto' }}></Text>
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+            }}
+          >
+            <View style={{ borderRight: 1, width: '30%' }}></View>
+            <View
+              style={{
+                width: '70%',
+                flexDirection: 'row',
+                textAlign: 'center',
+                borderRight: 1,
+                borderLeft: 1,
+                fontSize: 9,
+                height: 20,
+              }}
+            >
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ textAlign: 'left', marginVertical: 'auto' }}>TRUCK NUMBER</Text>
+              </View>
+              <View style={{ borderRight: 1, width: '75%' }}>
+                <Text style={{ textAlign: 'center', marginVertical: 'auto' }}></Text>
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+            }}
+          >
+            <View style={{ borderRight: 1, width: '30%' }}></View>
+            <View
+              style={{
+                width: '70%',
+                flexDirection: 'row',
+                border: 1,
+                fontSize: 9,
+                height: 20,
+              }}
+            >
+              <View style={{ borderRight: 1, width: '25%' }}>
+                <Text style={{ textAlign: 'left', marginVertical: 'auto' }}>ESCORTED BY</Text>
+              </View>
+              <View style={{ borderRight: 1, width: '75%' }}>
+                <Text style={{ textAlign: 'center', marginVertical: 'auto' }}></Text>
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              border: 1,
+              marginTop: '10px',
+              width: '100%',
+              height: '120px',
+            }}
+          >
+            <Text style={{ textAlign: 'left', padding: '5px' }}>Customer: </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              borderRight: 1,
+              borderLeft: 1,
+              borderBottom: 1,
+              fontSize: 9,
+              height: 60,
+            }}
+          >
+            <View style={{ width: '50%', borderRight: 1 }}>
+              <Text style={{ textAlign: 'left', width: '100%', padding: '5px' }}>
+                SUPERVISOR'S SIGNATURE
+              </Text>
+            </View>
+            <View style={{ width: '50%' }}>
+              <Text style={{ textAlign: 'left', width: '100%', padding: '5px' }}>
+                CONSIGNOR'S SIGNATURE
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ border: '1 solid red', marginTop: '20px' }} />
+        <FooterWithImage />
       </Page>
     </Document>
   );
