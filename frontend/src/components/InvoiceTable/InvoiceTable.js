ae2948fa2c13e7939576b5cc96858f28c28953b5
shaivpidadi <shaivpidadi@gmail.com>
2020-11-08 22:50:38 +0530

    Added sign in invoice

--- frontend/src/components/InvoiceTable/InvoiceTable.js
+++ frontend/src/components/InvoiceTable/InvoiceTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import FooterWithImage from '../FooterWithImage/FooterWithImage';
+import signImage from '../../assets/sign.jpg';
 
 const styles = StyleSheet.create({
   container: {
@@ -85,11 +86,11 @@ const InvoiceTable = ({ invoice }) => {
           </View>
 
           {invoiceDetails.map(({ expense, amount }) => {
-            if ( !!amount ) {
+            if (!!amount) {
               return (
                 <View style={{ flexDirection: 'row', alignContent: 'center', marginVertical: 'auto' }}>
-                <Text>{`${expense} ${amount}`}</Text>
-              </View>
+                  <Text>{`${expense} ${amount}`}</Text>
+                </View>
               )
             }
           })}
@@ -111,15 +112,15 @@ const InvoiceTable = ({ invoice }) => {
           <View style={{ flexDirection: 'row', marginTop: '15px' }}></View>
 
           {invoiceDetails.map(({ amount }) => {
-              if (!!amount) {
-                return (
-                  <View style={{ flexDirection: 'row', alignContent: 'center', marginVertical: 'auto' }}>
-                <View style={{ flexDirection: 'row', alignContent: 'center', margin: 'auto' }}>
-                  <Text>{amount}</Text>
+            if (!!amount) {
+              return (
+                <View style={{ flexDirection: 'row', alignContent: 'center', marginVertical: 'auto' }}>
+                  <View style={{ flexDirection: 'row', alignContent: 'center', margin: 'auto' }}>
+                    <Text>{amount}</Text>
+                  </View>
                 </View>
-              </View>
-                )
-              }
+              )
+            }
           })}
         </View>
       </View>
@@ -129,7 +130,7 @@ const InvoiceTable = ({ invoice }) => {
           style={{ width: '60%', border: 1, padding: 10, lineHeight: 1.2, flexDirection: 'row' }}
         >
           <View style={{ width: '70%' }}>
-            <Text style={{ textAlign: 'left' }}>PAYMENT AT: ${paymentCity}</Text>
+            <Text style={{ textAlign: 'left' }}>PAYMENT AT: {paymentCity}</Text>
           </View>
           <View style={{ width: '30%' }}>
             <Text style={{ textAlign: 'right' }}>Total</Text>
@@ -141,24 +142,25 @@ const InvoiceTable = ({ invoice }) => {
         </View>
       </View>
 
-      <View style={{ flexDirection: 'row', marginTop: 5 }}>
+      <View style={{ flexDirection: 'row', marginTop: 5, height: 100 }}>
         <View style={{ width: '60%', padding: 10, lineHeight: 1.2 }}>
           <Text style={{ textAlign: 'left' }}>{`Rupees in words: ${totalInWords}`}</Text>
         </View>
         <View style={{ width: '1%' }} />
         <View style={{ width: '39%', lineHeight: 1.2, padding: 10 }}>
           <Text style={{ marginHorizontal: 'auto' }}>Authorised Signatory</Text>
+          <Image style={{ width: '50px', height: '50px', marginLeft: '60px' }} src={signImage} />
         </View>
       </View>
 
       <View style={{ flexDirection: 'row', marginTop: 5, fontSize: 14 }}>
         <View style={{ width: '55%' }} />
         <View style={{ width: '6%', lineHeight: 1.2, padding: 10 }}>
-          <View style={{ height: 50 }} />
+          {/* <View style={{ height: 50 }} /> */}
           <Text style={{ textAlign: 'right' }}> FOR </Text>
         </View>
         <View style={{ width: '39%', lineHeight: 1.2, padding: 10 }}>
-          <View style={{ height: 50 }} />
+          {/* <View style={{ height: 50 }} /> */}
           <Text style={{ marginHorizontal: 'auto' }}>ACE RELOCATIONS</Text>
         </View>
       </View>
