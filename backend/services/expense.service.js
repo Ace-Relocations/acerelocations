b74cd0fb8ebc6c5c3db8f83c81d8770cb19a093a
dishant1997 <shahdishant28@gmail.com>
2020-12-05 22:15:01 +0530

    Changes to display dashboard

--- backend/services/expense.service.js
+++ backend/services/expense.service.js
@@ -28,7 +28,7 @@ module.exports = {
         try {
             let totalA = 0;
             for (i=0; i<expense.length; i++) {
-                totalA = totalA + expense[i].amount; 
+                totalA = totalA + parseInt(expense[i].amount); 
             }
             return totalA;
         } catch (err) {
